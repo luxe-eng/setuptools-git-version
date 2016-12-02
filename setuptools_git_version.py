15c5e3575a5dd8fafa670827e42f191a6413052a
David Macro <david.macro7@gmail.com>
2016-12-02 14:27:55 -0800

    always return tag if it exists

--- setuptools_git_version.py
+++ setuptools_git_version.py
@@ -37,7 +37,7 @@ def format_version(version, fmt=fmt):
     assert len(parts) in (3, 4)
     dirty = len(parts) == 4
     tag, count, sha = parts[:3]
-    if count == '0' and not dirty:
+    if count == '0':# and not dirty:
         return tag
     return fmt.format(tag=tag, commitcount=count, gitsha=sha.lstrip('g'))
 
